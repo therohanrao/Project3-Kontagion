bdd367de318abafe43cfd2dc0686f5ed9f7f86a0
therohanrao <59488758+therohanrao@users.noreply.github.com>
2020-02-18 14:21:47 -0800

    Test changed code

--- Kontagion/Kontagion/Actor.cpp
+++ Kontagion/Kontagion/Actor.cpp
@@ -2,3 +2,223 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+const double PI = 4 * atan(1);
+
+void Actor::activate()
+{
+
+}
+
+void Actor::setDead(bool state)
+{
+	m_dead = state;
+}
+
+bool Actor::isDead() const
+{
+	if (m_dead)
+		return true;
+	return false;
+}
+
+
+////////////////////////////////////////////////////////////
+/////////////////END IMPLEMENTATION/////////////////////////
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+/////////////////SOCRATES CLASS IMPLEMENTATION//////////////
+////////////////////////////////////////////////////////////
+
+void Socrates::doSomething()
+{
+    /*
+    class Socrates: public …
+ {
+ public:
+ virtual void doSomething()
+ {
+ Try to get user input (if any is available)
+ If the user pressed the LEFT key then
+ Move counterclockwise around the perimeter by 5 degrees
+ If the user pressed the RIGHT key then
+Move clockwise around the perimeter by 5 degrees
+ ...
+ If the user pressed space and Socrates has Sprays left, then
+Introduce a new Spray object into the Petri dish in front
+ of Socrates
+ ...
+ }
+ ...
+ };
+    */
+
+    /*
+    void Socrates::doSomething()
+{
+ ...
+ int ch;
+ if (getAPointerToMyStudentWorld()->getKey(ch))
+ {
+ // user hit a key during this tick!
+ switch (ch)
+ {
+ case KEY_PRESS_LEFT:
+ ... move Socrates counterclockwise ...;
+ break;
+ case KEY_PRESS_RIGHT:
+ ... move Socrates clockwise...;
+ break;
+ case KEY_PRESS_SPACE:
+ ... add spray in front of Socrates...;
+ break;
+ // etc…
+ }
+ }
+ ...
+}
+    */
+
+    //WHATS IN A GRAPHOBJECT ? ? ? THIS :
+    /*
+GraphObject(int imageID, double startX, double startY,
+int startDirection = 0, int depth = 0);
+
+// A NOTE ABOUT DEPTH:
+
+In
+Kontagion, all bacteria and Socrates are at depth 0, and all other game actors are at depth
+1, ensuring our active characters are in the foreground.
+//
+
+double getX() const; // in pixels (0-255)
+double getY() const; // in pixels (0-255)
+void moveTo(double x, double y); // in pixels (0-255)
+23
+
+// moveAngle() moves the actor the specified number of units in the
+// specified direction.
+
+void moveAngle(Direction angle, int units = 1);
+
+// getPositionInThisDirection() returns a new (x, y) location in the
+// specified direction and distance, based on the passed-in angle and the
+// GraphObject’s current (x, y) location.
+
+void getPositionInThisDirection(Direction angle, int units,
+ double &dx, double &dy);
+
+int getDirection() const; // in degrees (0-359)
+
+void setDirection(Direction d); // in degrees (0-359)
+*/
+
+
+    int ch;
+    if (getWorld()->getKey(ch))
+    {
+        int x = 0;
+        int y = 0;
+        switch (ch)
+        {
+
+        case KEY_PRESS_LEFT: //anti-clockwise movement
+
+            setDirection(getDirection() + 5);
+            setTheta();
+            polarToCartesian(x, y, m_theta);
+            x += VIEW_WIDTH / 2;
+            y += VIEW_HEIGHT / 2;
+            moveTo(x, y);
+
+            break;
+
+        case KEY_PRESS_RIGHT: //clockwise movement
+
+            setDirection(getDirection() - 5);
+            setTheta();
+            polarToCartesian(x, y, m_theta);
+            x += VIEW_WIDTH / 2;
+            y += VIEW_HEIGHT / 2;
+            moveTo(x, y);
+
+            break;
+
+        case KEY_PRESS_SPACE:
+            break;
+
+        case KEY_PRESS_ENTER:
+            break;
+
+        default:
+            break;
+        }
+
+        std::cout << "DIR" << getDirection() << std::endl;
+        std::cout << "THETA: " << m_theta << std::endl;
+        std::cout << "X: " << x << std::endl;
+        std::cout << "Y: " << y << std::endl << std::endl;
+    }
+
+
+
+}
+
+void Socrates::takeDamage()
+{
+
+}
+
+void Socrates::setTheta()
+{
+    int dir = getDirection();
+
+    if (dir < 180)
+        m_theta = 180 + dir;
+    else if (dir < 359)
+        m_theta = dir - 180;
+    else
+        std::cerr << "INVALID DIR SET" << std::endl;
+}
+
+void Socrates::polarToCartesian(int& x, int& y, int theta, int r)
+{
+    double rad = theta * (PI / 180);
+    x = r * (cos(rad));
+    y = r * (sin(rad));
+}
+
+////////////////////////////////////////////////////////////
+/////////////////END IMPLEMENTATION/////////////////////////
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+/////////////////DIRTPILE CLASS IMPLEMENTATION//////////////
+////////////////////////////////////////////////////////////
+
+void DirtPile::doSomething()
+{
+
+}
+
+////////////////////////////////////////////////////////////
+/////////////////END IMPLEMENTATION/////////////////////////
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+/////////////////END IMPLEMENTATION/////////////////////////
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+/////////////////END IMPLEMENTATION/////////////////////////
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+/////////////////END IMPLEMENTATION/////////////////////////
+////////////////////////////////////////////////////////////
--- Kontagion/Kontagion/StudentWorld.h
+++ Kontagion/Kontagion/StudentWorld.h
@@ -3,18 +3,40 @@
 
 #include "GameWorld.h"
 #include <string>
+#include <list>
+
 
 // Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class Actor;
+class Socrates;
 
 class StudentWorld : public GameWorld
 {
 public:
     StudentWorld(std::string assetPath);
+    virtual ~StudentWorld();
+
     virtual int init();
     virtual int move();
     virtual void cleanUp();
 
+    //move these three to private later:
+    void addPits(); 
+    void addDirt();
+    void addFood();
+
+    double distFromCenter(int x, int y) const;
+
 private:
+
+    Socrates* m_socrates;
+
+    std::list<Actor*> m_actors;
+    int m_numActors;
+
+    int m_centerX = VIEW_WIDTH/2;
+    int m_centerY = VIEW_HEIGHT/2;
 };
 
 #endif // STUDENTWORLD_H_
